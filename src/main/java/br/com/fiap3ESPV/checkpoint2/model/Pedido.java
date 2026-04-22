e78b9865bfa1c1d959df460f55797740ecdf6e73
Pedro <pedrohenriquesb02@gmail.com>
2026-04-22 17:39:45 -0300

    Criando a estrutura de pastas do projeto e Criando as classes de modelo de repository

---
+++ src/main/java/br/com/fiap3ESPV/checkpoint2/model/Pedido.java
@@ -0,0 +1,10 @@
+package br.com.fiap3ESPV.checkpoint2.model;
+
+import java.time.LocalDate;
+
+public class Pedido {
+    private Long id;
+    private String clienteNome;
+    private LocalDate dataPedido;
+    private Double valorTotal;
+}
